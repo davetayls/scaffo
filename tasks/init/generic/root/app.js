c799411b8f73f5c14c18b6078be11898fed206bd
davetayls <dave@the-taylors.org>
2012-10-25 12:07:19 +0100

    show options to customise environments

--- tasks/init/generic/root/app.js
+++ tasks/init/generic/root/app.js
@@ -10,6 +10,32 @@ var express = require('express'),
 var app = express();
 config.configure(express, app, __dirname);
 
+/**
+ * Environments
+ * you can specify specific options to test the code
+ * in different environments
+ */
+app.configure('development', function(){
+  app.locals.development = true;
+});
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+/**
+ * Custom Routes
+ * You can include custom routes for testing specific
+ * urls which might exist in the integrated site.
+
+    app.get('/service/people', function(req, res){
+      res.render('services/people', models.get(app));
+    });
+
+ */
+
+/**
+ * Start the server
+ */
 app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", port, app.settings.env);
 });
