3fc6b77d713628640e0562ae7f5d7d097d953980
davetayls <dave@the-taylors.org>
2012-11-02 15:54:09 +0000

    fix routing issues where regular files in the root were being rendered as views

--- tasks/init/generic/root/config/routes.js
+++ tasks/init/generic/root/config/routes.js
@@ -7,14 +7,14 @@ exports.configure = function(app, rootDir){
 	"use strict";
 
     // This is the default route which just loads the index page
-    app.get('/:template?', function(req, res){
+    app.get('/', function(req, res){
       var template = req.params.template || 'index';
       res.render(template, modelData.get(app));
     });
 
     // This picks up routes which point to a particular template
     // which sits inside the templates folder
-    app.get('/template/:layout?/:template', function(req, res){
+    app.get('/templates/:layout?/:template', function(req, res){
       var options = { layout: join(req.params.layout, 'layout.html') },
           template = req.params.template || 'index',
           view = join(req.params.layout, template);
