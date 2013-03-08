d23c959168fe55bffaf8169686f1ac1e0e40bd0d
davetayls <dave@the-taylors.org>
2013-03-08 13:49:00 +0000

    simplify template to just copy files

--- template.js
+++ template.js
@@ -1,40 +1,19 @@
-/*jshint node:true */
-exports.description = "Pogo: generic scaffolding";
-exports.notes = "Generates scaffolding for a generic project";
+'use strict';
+exports.description = 'Project scaffolding and prototyping';
 
+// Any existing file or directory matching this wildcard will cause a warning.
+exports.warnOn = '*';
+
+// The actual init template.
 exports.template = function(grunt, init, done) {
-  "use strict";
 
-  var _ = grunt.utils._;
-
-  _.extend(grunt.helper("prompt_for_obj"), {
-    css: {
-      message: "Which CSS pre-processor do you want? [ sass, stylus, none ]",
-      default: "sass"
-    },
-    responsive: {
-      message: 'Is this site responsive?',
-      default: 'yes'
-    },
-    templating: {
-      message: 'What templating library do you want? [ mustache, none ]',
-      default: 'mustache'
-    }
-  });
-
-  grunt.helper("prompt", {}, [
-
-    // grunt.helper("prompt_for", "css"),
-    // grunt.helper("prompt_for", "responsive"),
-    // grunt.helper("prompt_for", "templating")
-
-  ], function(err, props) {
+  init.process({}, [], function(err, props) {
 
     // Files to copy (and process).
     var files = init.filesToCopy(props);
 
     // Actually copy (and process) files.
-    init.copyAndProcess(files, props, {});
+    init.copyAndProcess(files, props);
 
     // All done!
     done();
